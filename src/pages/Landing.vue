47661a9a3b979a2c24a3a8aa84dbdebe9ccb9072
CC <caledoniaconsult@gmail.com>
2024-07-07 22:51:20 -0400

    wip - landing page pre-observer

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -14,6 +14,10 @@ const redirectCall = () => {
       <div class="slide">
         <div class="left">
           <img src="/nasa-Q1p7bh3SHj8-unsplash.jpg" class="landing-image"/>
+          <div class="container__testimonials">
+            <p class="testimonial">"Caledonia Consulting did an outstanding job for me on website design and development. Everything was customized to my requirements, and the resulting site is appealing, highly intuitive, and user-friendly. I would recommend Caledonia to anyone seeking expert guidance in setting up their business website."  07/06/2024
+            </p>
+          </div>
         </div>
         <div class="right">
           <div class="container__content--text">
@@ -42,7 +46,7 @@ const redirectCall = () => {
 <style scoped>
 
 .container__landing--desktop {
-  background-color: #008E97;
+  background-color: #FFF;
 }
 
 .container__content--container {
@@ -64,6 +68,18 @@ const redirectCall = () => {
 
   .left {
     width: 50vw;
+    position: relative;
+
+    .container__testimonials {
+      position: absolute;
+      bottom: 40%;
+      padding:  15px;
+      background-color: black;
+
+      .testimonial {
+        color: white;
+      }
+    }
   }
 
   .right {
@@ -72,12 +88,11 @@ const redirectCall = () => {
     align-items: center;
     display: flex;
 
-
     .company-title {
       font-family: "Nunito Sans", sans-serif;
       font-weight: 700;
       font-size: 40px;
-      color: white;
+      color: #008E97;
     }
   }
 }
